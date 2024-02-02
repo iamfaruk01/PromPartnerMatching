637950554920b8ba2311763dac9c3864d2bd548e
Faruk Khan <faruk.khan.cse@gmail.com>
2024-02-02 23:08:06 +0530

    settings.py modify

--- promMatch/settings.py
+++ promMatch/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-kd9y-oyl74l0lp0dz6tz1k3k7b4*9c36svroh=ttlb+a$a6^n)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,9 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
